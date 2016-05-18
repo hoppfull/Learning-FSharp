f57c5503c7cd3e53d7231c852814a2329887a926
Björn Kihlberg <hoppfull@gmail.com>
2016-05-19 00:34:34 +0200

    implemented Intermediate ex04 - operator overloading example

---
+++ Intermediate/ex04 - operator overloading/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace ex04___operator_overloading.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("ex04 - operator overloading")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("ex04 - operator overloading")>]
+[<assembly: AssemblyCopyright("Copyright ©  2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("54800d55-0f09-45aa-979d-d80111653c4f")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ Intermediate/ex04 - operator overloading/Program.fs
@@ -0,0 +1,40 @@
+﻿type Complex =
+    { a: double; bi: double}
+    static member (+) (l: Complex, r: Complex) =
+        { a = l.a + r.a; bi = l.bi + r.bi }
+    static member (-) (l: Complex, r:Complex) =
+        { a = l.a - r.a; bi = l.bi - r.bi }
+    static member (*) (l: Complex, r:Complex) =
+        { a = l.a * r.a - l.bi * r.bi; bi = l.a * r.bi * 2. }
+
+let z0 = { a = 0.; bi = 1. }
+let z1 = { a = 2.; bi = 1. }
+let z2 = { a = 1.; bi = 0. }
+
+printfn "%A" (z0 + z1) // cmd: { a = 2.0; bi = 2.0 }
+printfn "%A" (z0 + z0) // cmd: { a = 0.0; bi = 2.0 }
+printfn "%A" (z0 * z0) // cmd: { a = -1.0; bi = 0.0 }
+printfn "%A" (z2 * z2) // cmd: { a = 1.0; bi = 0.0 }
+printfn "%A" (z1 * z2) // cmd: { a = 2.0; bi = 0.0 }
+printfn "%A" (z0 * z2) // cmd: { a = 0.0; bi = 0.0 }
+printfn "%A" (z1 * z1) // cmd: { a = 3.0; bi = 4.0 }
+
+// defining new operators:
+// available characters: ! % & * + - . / < = > ? @ ^ | ~
+
+open System.Text.RegularExpressions
+// infix operator if two arguments:
+let (@?) pattern input = Regex.IsMatch(input, pattern)
+
+printfn "%b" ("[0-9]+" @? "hello" ) // cmd: false
+printfn "%b" ("[a-z]+" @? "hello" ) // cmd: true
+printfn "%b" ("\A[a-z]+\Z" @? "hello " ) // cmd: false
+// oh my tits this is so cool!
+
+// prefix if one argument:
+let (!) s:string = s + "zorz!!!1!one!!"
+
+printfn "%s" !"hello" // cmd: hellozorz!!!1!one!!
+printfn "%s" !"cat" // cmd: catzorz!!!1!one!!
+printfn "%s" !"laser" // cmd: laserzorz!!!1!one!!
+// christing mcbollock this is awesome!
