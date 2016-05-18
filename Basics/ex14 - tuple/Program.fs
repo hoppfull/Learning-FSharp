9b8af815564502d083e167e6f2e8a9876ce67d6d
Björn Kihlberg <hoppfull@gmail.com>
2016-05-18 21:52:12 +0200

    implemented Basics ex14 - tuple example

---
+++ Basics/ex14 - tuple/Program.fs
@@ -0,0 +1,11 @@
+﻿// a 3-tuple of three ints:
+let t0 = 1, 1, 0
+// a 3-tuple of two floats and a string:
+let t1 = 3.14, 7., "hello"
+// a tuple of two chars:
+let t2 = 'a', 'b'
+// functions fst and snd only work on 2-tuples
+printfn "%c" (fst t2) // cmd: a
+printfn "%c" (snd t2) // cmd: b
+
+// use "deconstruction" (see ex15) to retrieve elements from tuples!
