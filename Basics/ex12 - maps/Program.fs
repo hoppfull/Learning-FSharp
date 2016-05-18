ef2ad90df1163324670b820169ee8afca33ce216
Björn Kihlberg <hoppfull@gmail.com>
2016-05-18 21:30:33 +0200

    implemented Basics ex12 - maps example

---
+++ Basics/ex12 - maps/Program.fs
@@ -0,0 +1,11 @@
+﻿// Maps are collections of key-value pairs like dictionaries
+
+let lmap =
+    Map.empty
+        .Add("Han Solo", "Chewbacca")
+        .Add("Cat", "Dog")
+        .Add("Black", "White")
+
+printfn "%s" lmap.["Han Solo"] // cmd: Chewbacca
+printfn "%s" lmap.["Cat"] // cmd: Dog
+printfn "%s" lmap.["Black"] // cmd: White
