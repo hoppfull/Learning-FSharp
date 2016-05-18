ab32296633000033af0e2d81602af84b5fdc7b01
Björn Kihlberg <hoppfull@gmail.com>
2016-05-18 17:43:40 +0200

    implemented Basics ex02 - variables example

---
+++ Basics/ex02 - variables/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace ex02___variables.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("ex02 - variables")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("ex02 - variables")>]
+[<assembly: AssemblyCopyright("Copyright ©  2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("47588a3c-2606-41b0-a57d-b36c03eb6bae")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ Basics/ex02 - variables/Program.fs
@@ -0,0 +1,9 @@
+﻿// default variables are immutable:
+let x = 5.5 // strong implicit typing, x is a float
+
+// mutable variables are mutable:
+let mutable z = 5
+z <- 42 // assign new value to mutable variable z
+
+printfn "%.2f" x
+printfn "%i" z
