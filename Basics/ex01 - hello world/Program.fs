93fb81b35838571712ee7e2c9267c78d64441a1e
Björn Kihlberg <hoppfull@gmail.com>
2016-05-18 17:35:33 +0200

    implemented Basics ex01 - hello world example

--- Basics/ex01 - hello world/Program.fs
+++ Basics/ex01 - hello world/Program.fs
@@ -1,1 +1,25 @@
 ﻿printfn "Hello world!"
+
+// string:
+printfn "%s" "hello"
+
+// int:
+printfn "%i" 42
+
+// float:
+printfn "%f" 3.14
+
+// float with given precision
+printfn "%.1f" 3.14
+
+// tuples, records, biginteger and union types:
+printfn "%A" (5, 6)
+
+// Any object:
+printfn "%O" 4
+
+// bool:
+printfn "%b" true
+
+// exponential format
+printfn "%e" 100.0
