6db484762a0af932366b155b249b8fd688fd146b
Björn Kihlberg <hoppfull@gmail.com>
2016-05-18 20:04:42 +0200

    implemented Basics ex07 - recursion example

--- Basics/ex07 - recursion/Program.fs
+++ Basics/ex07 - recursion/Program.fs
@@ -1,1 +1,16 @@
-﻿
+﻿// CLR doesn't have tail call optimization (TCO)
+// We declare a function as recursive to force TCO:
+let rec f x sum =
+    if x > 0 then f (x - 1) (sum + x)
+    else sum
+
+f 10 0 |> printfn "%i" // cmd: 55
+
+// A pattern for hard coding initial values:
+let g =
+    let rec h x sum =
+        if x > 0 then f (x - 1) (sum + x)
+        else sum
+    fun x -> h x 0
+
+g 10 |> printfn "%i" // cmd: 55
