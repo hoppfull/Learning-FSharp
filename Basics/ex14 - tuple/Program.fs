a123b644ebe2780bf29356c784c505fdb758949d
Björn Kihlberg <hoppfull@gmail.com>
2016-05-18 22:02:38 +0200

    appended further explanation of deconstruction to ex14 example

--- Basics/ex14 - tuple/Program.fs
+++ Basics/ex14 - tuple/Program.fs
@@ -8,4 +8,23 @@ let t2 = 'a', 'b'
 printfn "%c" (fst t2) // cmd: a
 printfn "%c" (snd t2) // cmd: b
 
-// use "deconstruction" (see ex15) to retrieve elements from tuples!
+// use "deconstruction" to retrieve elements from tuples!
+
+(* Deconstruction is a technique for extracting data
+from a datastructure and is used a lot in F# programs.
+Useful for simplifying code and readability.*)
+
+// a 3-tuple:
+let t = "hello", 7., 'x'
+let s, f, c = t
+printfn "%s" s // cmd: hello
+printfn "%.2f" f // cmd: 7.00
+printfn "%c" c // cmd: x
+
+let myFunc (a, b, c:char) =
+    printfn "%s" (a + c.ToString())
+    b * 2.
+
+printfn "%.1f" (myFunc t)
+// cmd: hellox
+// cmd: 14.0
