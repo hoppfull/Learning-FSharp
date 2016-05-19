580da6eaed2ccf772f7b2ffede2bf617f018bd7b
Björn Kihlberg <hoppfull@gmail.com>
2016-05-19 20:51:35 +0200

    implemented OOP ex05 - inheritance example

---
+++ OOP/ex05 - inheritance/Program.fs
@@ -0,0 +1,10 @@
+﻿type MyClassA(x) =
+    member this.x = x
+
+type MyClassB(x, y) =
+    inherit MyClassA(x)
+    member this.y = y
+    member this.sprintf = sprintf "(%i, %i)" x y
+
+let myObj = new MyClassB(7, 42)
+printfn "%s" <| myObj.sprintf // cmd: (7, 42)
