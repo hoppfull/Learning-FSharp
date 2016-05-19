88563b222bbaa063f1d1cba9bf9ba89cea18a3e7
Björn Kihlberg <hoppfull@gmail.com>
2016-05-19 05:41:11 +0200

    implemented Intermediate ex08 - tasks example

---
+++ Intermediate/ex08 - tasks/Program.fs
@@ -0,0 +1,18 @@
+﻿open System.Threading
+
+let fAsync msg = async {
+    do! Async.Sleep 4000
+    return msg
+}
+
+printfn "starting task"
+let task = fAsync "task done!" |> Async.StartAsTask
+
+printfn "doing work"
+Thread.Sleep 3500
+printfn "work done"
+
+printfn "%s" task.Result
+
+(* Tasks start doing work right away
+and parent thread continues with its life*)
