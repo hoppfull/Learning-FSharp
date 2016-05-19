5c32f1d1a515e9560177e9353512a2cef3605923
Björn Kihlberg <hoppfull@gmail.com>
2016-05-19 20:41:57 +0200

    implemented OOP ex04 - static members example

---
+++ OOP/ex04 - static members/Program.fs
@@ -0,0 +1,22 @@
+﻿// the singleton pattern with static member:
+type MyClass(x) =
+    do printf "created new instance! "
+    static let mutable instance:MyClass Option = None
+    static member Instance
+        with get() = match instance with
+                     | None   -> instance <- Some <| new MyClass(5)
+                                 instance.Value
+                     | Some i -> i
+    member this.x = x
+
+// a default instance is created:
+printfn "%i" <| MyClass.Instance.x // cmd: created new instance! 5
+printfn "%i" <| MyClass.Instance.x // cmd: 5
+printfn "%i" <| MyClass.Instance.x // cmd: 5
+printfn "%i" <| MyClass.Instance.x // cmd: 5
+
+// custom instance is created:
+let myObj = new MyClass(7)
+printfn "%i" <| myObj.x // cmd: created new instance! 7
+
+// I think this is ideal for mocking. Not sure if I've done it right.
