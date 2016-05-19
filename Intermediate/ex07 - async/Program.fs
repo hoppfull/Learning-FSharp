e93a97aa578a59221e103fdfd92a6a0ca24e0be3
Björn Kihlberg <hoppfull@gmail.com>
2016-05-19 04:53:44 +0200

    implemented Intermediate ex07 - async example

---
+++ Intermediate/ex07 - async/Program.fs
@@ -0,0 +1,16 @@
+﻿let fAsync x = async {
+    // let! do! and return! will block thread until done
+    do! Async.Sleep 5000
+    // heavy calculation:
+    return x * 2
+}
+
+// these are not even evaluated yet:
+let resultAsync =
+    [1..10]
+    |> List.map fAsync
+    |> Async.Parallel
+
+// threads start and finish here:
+for result in Async.RunSynchronously resultAsync do
+    printfn "%i" result
