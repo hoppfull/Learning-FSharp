14db7ccec599f17906d161813411f9edace7c91d
Björn Kihlberg <hoppfull@gmail.com>
2016-05-19 20:11:31 +0200

    implemeneted OOP ex03 - secondary constructors example

---
+++ OOP/ex03 - secondary constructors/Program.fs
@@ -0,0 +1,14 @@
+﻿type MyClass() =
+    do printfn "hello"
+
+    new(a) =
+        printf "%s" a
+        MyClass()
+
+    new(x) =
+        printf "%i" x
+        MyClass()
+
+let myObj0 = new MyClass() // cmd: hello
+let myObj1 = new MyClass(42) // cmd: 42hello
+let myObj2 = new MyClass("whatsup") // cmd: whatsuphello
