e6a8252879776cd8996ddd9ffa9c3988c38d1a56
Björn Kihlberg <hoppfull@gmail.com>
2016-05-19 21:13:47 +0200

    implemented OOP ex06 - abstract classes example

---
+++ OOP/ex06 - abstract classes/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace ex06___abstract_classes.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("ex06 - abstract classes")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("ex06 - abstract classes")>]
+[<assembly: AssemblyCopyright("Copyright ©  2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("2dca4287-715b-4fe6-9d7e-f3bd0455fab2")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ OOP/ex06 - abstract classes/Program.fs
@@ -0,0 +1,29 @@
+﻿(* if not all abstract members are implemented, then class has to be
+abstract which is defined with the AbstractClass-decorator: *)
+[<AbstractClass>]
+type MyClassA() =
+    abstract member x: double
+    abstract member y: double
+    abstract member f: double -> double
+
+    // members can but must not be defined in an abstract class:
+    default this.f x = x * 2.
+
+type MyClassB(name) =
+    member this.name = name
+    abstract member sprintf: string
+    // all abstract members must be defined in a non-abstract class:
+    default this.sprintf = sprintf "%s" this.name
+
+type MyClassBSub(name) =
+    inherit MyClassB(name)
+    // abstract members can be overriden with new implementation:
+    override this.sprintf = sprintf "Hello, %s!" base.sprintf
+
+type MyClassASub(x, y) =
+    inherit MyClassA()
+
+    // all abstract members with no defaults has to be implemented:
+    override this.x = x
+    override this.y = y
+    override this.f z = base.f (x + y + z)
