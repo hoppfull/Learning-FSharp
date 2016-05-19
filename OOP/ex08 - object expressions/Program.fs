b54a7dc7b095335e90acb012b5ef05af5f3c5c2d
Björn Kihlberg <hoppfull@gmail.com>
2016-05-19 21:47:24 +0200

    implemented OOP ex08 - object expressions example

---
+++ OOP/ex08 - object expressions/Program.fs
@@ -0,0 +1,23 @@
+﻿type IPoint =
+    abstract member x: int
+    abstract member y: int
+
+let f (o:IPoint) =
+    o.x + o.y
+
+// define new object that implements IPoint inline:
+{ new IPoint with
+    member this.x = 7
+    member this.y = 5 }
+|> f
+|> printfn "%i" // cmd: 12
+
+// factory pattern + interface implementation for IPoint:
+let newPoint x y =
+    { new IPoint with 
+        member this.x = x
+        member this.y = y }
+
+newPoint 42 18
+|> f
+|> printfn "%i" // cmd: 60
