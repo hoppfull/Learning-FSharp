63b05e2666abd2d0f93498245817faabb6dbe249
Björn Kihlberg <hoppfull@gmail.com>
2016-05-19 17:09:44 +0200

    implemented Intermediate ex10 - modules example

---
+++ Intermediate/ex10 - modules/MyModule.fs
@@ -0,0 +1,7 @@
+﻿// A module is basically a static class and can be treated like one
+module MyModule
+
+// all members are public unless explicitly defined otherwise:
+let private g x = x - 2
+let f x = x * 2 |> g
+
---
+++ Intermediate/ex10 - modules/Program.fs
@@ -0,0 +1,7 @@
+﻿open MyModule
+(* Cyclic dependencies are not allowed in F#. This is a feature to enforce
+clean code architecture. Since this file is dependent on MyModule, MyModule
+can't access anything in this file. In order to set correct compilation
+order, set file order of appearance in solution explorer.*)
+
+printfn "%i" (f 4) // cmd: 6
