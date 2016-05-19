92356838b49b81c0ff18fc7d0a9c00e475ea899c
Björn Kihlberg <hoppfull@gmail.com>
2016-05-19 17:31:49 +0200

    implemented Intermediate ex11 - namespaces example

---
+++ Intermediate/ex11 - namespaces/MyNamespace.fs
@@ -0,0 +1,15 @@
+﻿namespace MyNamespace
+
+type MyType = int -> int -> int
+
+type MyUnion =
+    | Huey
+    | Dewey
+    | Louie
+
+type MyRecordType = { x: int; y: int }
+
+module MyModule =
+    let f x = x * 2
+
+// namespaces can only contain modules and types
---
+++ Intermediate/ex11 - namespaces/Program.fs
@@ -0,0 +1,16 @@
+﻿open MyNamespace
+
+let g:MyType = fun x y -> x + y
+
+printfn "%i" (MyModule.f 55) // cmd: 110s
+
+let union = MyUnion.Huey
+
+match union with
+| Huey  -> g 5 12
+| Dewey -> g 7 6
+| Louie -> g 9 0
+|> printfn "%i" // cmd: 17
+
+{ x = 7; y = 42 }
+|> printfn "%A" // cmd: { x = 7; y = 42 }
