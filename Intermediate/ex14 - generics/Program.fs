1bf41a0a9c9a6eb5576d690e8f3a5081aff21441
Björn Kihlberg <hoppfull@gmail.com>
2016-05-19 19:10:48 +0200

    implemented Intermediate ex14 - generics example

---
+++ Intermediate/ex14 - generics/Program.fs
@@ -0,0 +1,12 @@
+﻿// Generics are slow but allow us to simplify code
+
+let f<'a> (x:'a) = x
+
+let inline g x y = x + y
+
+f 42 |> printfn "%i" // cmd: 42
+f 7. |> printfn "%.1f" // cmd: 7.0
+g 3 5 |> printfn "%i" // cmd: 8
+g 7. 6. |> printfn "%.1f" // cmd: 13.0
+
+// I think I should try to limit usage of generics. I'm not sure. We'll see...
