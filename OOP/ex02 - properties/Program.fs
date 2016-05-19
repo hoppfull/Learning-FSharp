42fb08289542da19c100be4ca275c71d25610746
Björn Kihlberg <hoppfull@gmail.com>
2016-05-19 20:05:12 +0200

    implemented OOP ex02 - properties example

---
+++ OOP/ex02 - properties/Program.fs
@@ -0,0 +1,19 @@
+﻿type MyClass() =
+    let mutable myPropB = 0
+    let mutable myPropC = 0
+    // immutable property (only get method):
+    member this.MyPropA = 5
+    // mutable property (both get and set methods):
+    member this.MyPropB
+        with get() = myPropB
+        and set value = myPropB <- value
+
+    // mutable property (but with private set):
+    member this.MyPropC
+        with get() = myPropC
+        and private set value = myPropC <- value
+
+    // automatic immutable property:
+    member val MyPropD = 0
+    // automatic mutable property:
+    member val MyPropE = 0 with get, set
