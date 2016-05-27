b540203e57092daf3ed1ebc53f434d5073e8d74c
Björn Kihlberg <hoppfull@gmail.com>
2016-05-27 08:30:25 +0200

    created some MVVM examples

---
+++ MVVM/ex01 - MVVM/Program.fs
@@ -0,0 +1,23 @@
+﻿open System
+open System.Windows
+open System.Windows.Markup
+
+let ReadXaml filename =
+    IO.File.ReadLines filename
+        |> String.concat "\n"
+        |> XamlReader.Parse
+        :?> Window
+
+type Model = { Message: string }
+
+type ViewModel() =
+    member this.Model = { Message = "Hello MVVM!" }
+    member this.Message = this.Model.Message
+
+let View =
+    let w = ReadXaml "View.xaml"
+    w.DataContext <- new ViewModel()
+    w
+
+[<EntryPoint; STAThread>]
+let main args = (new Application()).Run View
