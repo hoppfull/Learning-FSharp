0c6507706a1e1cf775b1142d9f6bfe4416d76c84
bjorn kihlberg <hoppfull@gmail.com>
2016-07-18 11:44:59 +0200

    implemented basic jwt example

---
+++ WebServer/ex - JWT/Program.fs
@@ -0,0 +1,13 @@
+﻿open JWT
+
+let key = "scrooge"
+
+type point = { x:int; y:int; s:string }
+
+let token = JsonWebToken.Encode({ x=5; y=9; s="hello" }, key, JwtHashAlgorithm.HS256)
+
+printfn "%s" token
+
+let json = JsonWebToken.Decode(token, key)
+
+printfn "%s" json
